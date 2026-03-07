ab8eac256ecd0e293631c32feaf1c5c525827f39
Rurutia1027 <rurutia1027@gmail.com>
2026-03-07 17:07:24 +0800

    remove no use files and docs, gonna change repo name into Cloud Native EDA Lab

--- docker/postgres-init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-for db in customer fraud notification; do
-  psql -v ON_ERROR_STOP=1 --username "postgres" -tc "SELECT 1 FROM pg_database WHERE datname = '$db'" | grep -q 1 \
-    || psql -v ON_ERROR_STOP=1 --username "postgres" -c "CREATE DATABASE $db OWNER amigoscode"
-done
