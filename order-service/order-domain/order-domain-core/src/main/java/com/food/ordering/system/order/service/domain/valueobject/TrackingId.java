9c0aad37a0baa3f08d3c82aa19c6137af23dfe27
Rurutia1027 <rurutia1027@gmail.com>
2026-05-07 11:31:28 +0800

    add order domain inner order domain core classes includes events, entity, valueobject, and exception

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/valueobject/TrackingId.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/valueobject/TrackingId.java
@@ -1,4 +1,11 @@
 package com.food.ordering.system.order.service.domain.valueobject;
 
-public class TrackingId {
+import com.food.ordering.system.domain.valueobject.BaseId;
+
+import java.util.UUID;
+
+public class TrackingId extends BaseId<UUID> {
+    public TrackingId(UUID value) {
+        super(value);
+    }
 }
