e7503cb2b151997ed36ef0181d5ba2f0d62e03c4
Rurutia1027 <rurutia1027@gmail.com>
2026-05-21 22:26:04 +0800

    add shell script for send request to order domain rest endpoint

---
+++ bin/create-order.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+curl -sS -X POST 'http://localhost:18081/orders' \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/vnd.api.v1+json' \
+  -d '{
+  "customerId": "d215b5f8-0249-4dc5-89a3-51fd148cfb41",
+  "restaurantId": "d215b5f8-0249-4dc5-89a3-51fd148cfb45",
+  "price": 200.00,
+  "address": {
+    "street": "street_1",
+    "postalCode": "1000AB",
+    "city": "Paris"
+  },
+  "items": [
+    {
+      "productId": "d215b5f8-0249-4dc5-89a3-51fd148cfb48",
+      "quantity": 1,
+      "price": 50.00,
+      "subTotal": 50.00
+    },
+    {
+      "productId": "d215b5f8-0249-4dc5-89a3-51fd148cfb48",
+      "quantity": 3,
+      "price": 50.00,
+      "subTotal": 150.00
+    }
+  ]
+}'
